7157ce2cf2c346c8c888d11bd9c959e664b69c85
Lordmatics <niallmaple@yahoo.co.uk>
2017-06-16 13:13:50 +0100

    Change Scene Tool - Useful for later down the development

---
+++ StoryGameDemo/Assets/Editor/SceneTools/ChangeSceneEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public class ChangeSceneEditor : Editor
+{
+
+    [MenuItem("OpenScene/Main")]
+    public static void OpenMainScene()
+    {
+        OpenScene("Main");
+    }
+
+    static void OpenScene(string name)
+    {
+        if(EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+        {
+            EditorSceneManager.OpenScene("Assets/Scenes/" + name + ".unity");
+        }
+    }
+}
