8816605fc4b0db22e1730744e5cda7e25a1fc110
Lordmatics <niallmaple@yahoo.co.uk>
2017-06-19 14:20:18 +0100

    K, Got some semblance of videos working in canvas space
    This took a while.... lol
    Using one Render Texture to portray the video onto canvas space, and
    what you will have to do is place your video into the resources folder
    and load it dynamically during runtime

---
+++ StoryGameDemo/Assets/Scripts/Test/VideoTest.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+[AddComponentMenu("Scripts/Test/VideoTest")]
+[RequireComponent(typeof(AudioSource))]
+public class VideoTest : MonoBehaviour
+{
+
+    [SerializeField]
+    private RawImage image;
+
+    [SerializeField]
+    private VideoPlayer videoPlayer;
+
+    [SerializeField]
+    private AudioSource audioSource;
+
+    //[SerializeField]
+    //private VideoSource videoSource;
+
+    //[SerializeField]
+    //private string videoURL;
+
+    [SerializeField]
+    private VideoClip videoClip;
+
+
+    public MovieTexture movie;
+
+    void Start()
+    {
+        //videoPlayer = gameObject.AddComponent<UnityEngine.Video.VideoPlayer>();
+        videoPlayer.clip = videoClip;// AssetDatabase.LoadAssetAtPath<UnityEngine.Video.VideoClip>("Assets/BigBuckBunny.mp4");
+        videoPlayer.isLooping = true;
+        
+        //videoPlayer.target = UnityEngine.Video.VideoTarget.CameraFrontPlane;
+        //videoPlayer.alpha = 0.5f;
+        videoPlayer.loopPointReached += EndReached;
+        videoPlayer.Play();
+    }
+    void EndReached(UnityEngine.Video.VideoPlayer vPlayer)
+    {
+        Debug.Log("End reached!");
+    }
+
+    void Update()
+    {
+        Debug.Log("Frame " + videoPlayer.frame);
+    }
+
+ //   void Start ()
+ //   {
+ //       Application.runInBackground = true;
+ //       videoPlayer.clip = videoClip;
+ //       image.texture = videoPlayer.texture;
+ //       audioSource = GetComponent<AudioSource>() == null ? gameObject.AddComponent<AudioSource>() : GetComponent<AudioSource>();
+ //       audioSource.playOnAwake = false;
+ //       videoPlayer.audioOutputMode = VideoAudioOutputMode.AudioSource;
+ //       videoPlayer.EnableAudioTrack(0, true);
+ //       videoPlayer.SetTargetAudioSource(0, audioSource);
+
+ //       videoPlayer.Play();
+ //       audioSource.Play();
+ //       //StartCoroutine(PlayVideo());
+	//}
+	
+    //public IEnumerator PlayVideo()
+    //{
+        //WWW www = new WWW(videoURL);
+        //videoPlayer = gameObject.AddComponent<VideoPlayer>();
+        //audioSource = gameObject.AddComponent<AudioSource>();
+
+        //videoPlayer.playOnAwake = false;
+        //audioSource.playOnAwake = false;
+        //audioSource.Pause();
+
+        //videoPlayer.source = VideoSource.VideoClip;
+        //videoPlayer.clip = videoClip;
+        //videoPlayer.Play();
+        //yield return null;
+        //videoPlayer.url = videoURL;// www.url;
+
+        //videoPlayer.audioOutputMode = VideoAudioOutputMode.AudioSource;
+
+        //videoPlayer.EnableAudioTrack(0, true);
+        //videoPlayer.SetTargetAudioSource(0, audioSource);
+
+        //videoPlayer.Prepare();
+
+        //WaitForSeconds waitTime = new WaitForSeconds(1);
+        //while(!videoPlayer.isPrepared)
+        //{
+        //    Debug.Log("Preparing Video");
+        //    yield return waitTime;
+        //    break;
+        //}
+        //Debug.Log("Finished Preparations");
+
+        //image.texture = videoPlayer.texture;
+
+        //videoPlayer.Play();
+
+        //audioSource.Play();
+    //}
+
+}
---
+++ StoryGameDemo/Assets/Scripts/Video/AssignCurrentRenderTexture.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+using UnityEngine.EventSystems;
+
+
+// http://video.online-convert.com/convert-to-mp4
+// Converter to MP4. .flv didn't work, but mp4 does
+
+[AddComponentMenu("Scripts/Video/AssignRenderTexture")]
+[RequireComponent(typeof(RawImage))]
+[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(VideoPlayer))]
+public class AssignCurrentRenderTexture : MonoBehaviour, IPointerDownHandler
+{
+    private RawImage image;
+    private AudioSource audioSource;
+    private VideoPlayer videoPlayer;
+
+    public static AssignCurrentRenderTexture instance;
+
+    public Action OnVideoEnd;
+
+    bool bIsPaused = false;
+    bool bFinishedCurrentClip = false;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void OnEnable()
+    {
+        OnVideoEnd += Finished;    
+    }
+
+    private void OnDisable()
+    {
+        OnVideoEnd -= Finished;   
+    }
+
+    void Finished()
+    {
+        Debug.Log("Finished");
+        bFinishedCurrentClip = true;
+        videoPlayer.Stop();
+        audioSource.Stop();
+        SetAlpha(0.0f);
+    }
+
+    void Start ()
+    {
+        image = GetComponent<RawImage>();
+        audioSource = GetComponent<AudioSource>();
+        videoPlayer = GetComponent<VideoPlayer>();
+        videoPlayer.audioOutputMode = VideoAudioOutputMode.AudioSource;
+        videoPlayer.EnableAudioTrack(0, true);
+        videoPlayer.SetTargetAudioSource(0, audioSource);
+
+
+        PlayVideoAt("Videos/Test/TestA");
+        //double length = videoPlayer.clip.length;
+        //Debug.Log("Length Of Video: " + length + "s");
+    }
+
+    public void PlayVideoAt(string filePath)
+    {
+        //"Videos/Test/Video"
+        videoPlayer.clip = (VideoClip)Resources.Load(filePath);
+        videoPlayer.Play();
+        StartCoroutine(OnVideoFinished(OnVideoEnd));
+        audioSource.Play();
+        RenderTexture texture = (RenderTexture)Resources.Load("Videos/Test/VideoRT");
+        SetRenderTexture(texture);
+        SetAlpha(1.0f);
+    }
+
+    private IEnumerator OnVideoFinished(Action Callback)
+    {
+        while(videoPlayer.isPlaying || bIsPaused)
+        {
+            yield return 0;
+        }
+        if(Callback != null)
+        {
+            Callback();
+        }
+        yield break;
+    }
+    void ChangeVid()
+    {
+        videoPlayer.Stop();
+        audioSource.Stop();
+        SetAlpha(0.0f);
+        videoPlayer.clip = (VideoClip)Resources.Load("Videos/Test/Video2");
+        videoPlayer.Play();
+        audioSource.Play();
+        SetAlpha(1.0f);
+    }
+
+    public void SetRenderTexture(RenderTexture texture)
+    {
+        image.texture = texture;
+    }
+
+    public void SetAlpha(float alpha)
+    {
+        Color curColor = image.color;
+        curColor.a = alpha;
+        image.color = curColor;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        PauseOrResume();
+    }
+
+    void PauseOrResume()
+    {
+        switch(bIsPaused)
+        {
+            case true:
+                Resume();
+                break;
+            case false:
+                Pause();
+                break;
+        }
+    }
+
+    void Resume()
+    {
+        bIsPaused = false;
+        videoPlayer.Play();
+    }
+
+    void Pause()
+    {
+        // If you click on video, once its finished, it will play from beginning
+        if (bFinishedCurrentClip)
+        {
+            SetAlpha(1.0f);
+            Resume();
+            return;
+        }
+        bIsPaused = true;
+        videoPlayer.Pause();
+    }
+}
+
+
